39965b426f9d27e491a90daa0bbf87c3c1620902
diegoantonioalmontezarzuela@gmail.com <diegoantonioalmontezarzuela@gmail.com>
2017-10-06 10:22:01 +0200

    En este commit he cambiado que el sol sube y baja y tengo un suelo de color verde

--- Picture.java
+++ Picture.java
@@ -15,6 +15,7 @@ public class Picture
     private Square window;
     private Triangle roof;
     private Circle sun;
+    private Circle suelo;
 
     /**
      * Constructor for objects of class Picture
@@ -29,6 +30,14 @@ public class Picture
      */
     public void draw()
     {
+        suelo = new Circle();
+        suelo.changeColor("green");
+        suelo.moveHorizontal(-500);
+        suelo.moveVertical(140);
+        suelo.changeSize(1000);
+        suelo.makeVisible();
+        
+        
         wall = new Square();
         wall.moveHorizontal(-140);
         wall.moveVertical(20);
@@ -54,6 +63,9 @@ public class Picture
         sun.moveVertical(-40);
         sun.changeSize(80);
         sun.makeVisible();
+        sun.slowMoveVertical(300);
+        sun.slowMoveVertical(-300);
+        
     }
 
     /**
